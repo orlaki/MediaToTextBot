5057f3d7c891d33469f8dd394d521611cc9de1cb
orlaki <lakicalinuur@icloud.com>
2025-11-29 10:33:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ DOWNLOADS_DIR = os.environ.get("DOWNLOADS_DIR", "./downloads")
 os.makedirs(DOWNLOADS_DIR, exist_ok=True)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-USER_FREE_USAGE = int(os.environ.get("USER_FREE_USAGE", 1))
+USER_FREE_USAGE = int(os.environ.get("USER_FREE_USAGE", 2))
 
 def parse_keys(s):
     if not s:
